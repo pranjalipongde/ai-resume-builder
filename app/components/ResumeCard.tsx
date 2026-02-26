db8a1880d62d3a2833a98e85e685106574edd7ad
pranjalipongde09 <pranjalipongde9@gmail.com>
2026-02-26 17:36:43 +0530

    added navbar

---
+++ app/components/ResumeCard.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router";
+
+const ResumeCard = ({ resume }: { resume: Resume }) => {
+  return (
+    <Link
+      to={`/resume/${resume.id}`}
+      className="resume-card animate-in fade-in duration-1000"
+    >
+      ResumeCard
+    </Link>
+  );
+};
+
+export default ResumeCard;
