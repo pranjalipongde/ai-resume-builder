866a6b599da4a14802244ac9f5706d2c0ed4e43d
pranjalipongde09 <pranjalipongde9@gmail.com>
2026-02-26 21:11:26 +0530

    added resume card project

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -14,20 +14,20 @@ export default function Home() {
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
+
       <section className="main-section">
         <div className="page-heading">
           <h1>Track Your Applications & Resume Ratings</h1>
           <h2>Review Your submissions and check AI-powered feedback.</h2>
         </div>
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}{" "}
       </section>
-
-      {resumes.length > 0 && (
-        <div className="resumes-section">
-          {resumes.map((resume) => (
-            <ResumeCard key={resume.id} resume={resume} />
-          ))}
-        </div>
-      )}
     </main>
   );
 }
