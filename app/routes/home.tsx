7960aeb7b905fb131f5fcf0d75816d55763b279a
pranjalipongde09 <pranjalipongde9@gmail.com>
2026-03-05 16:54:00 +0530

    Designed homePage, navebar and resumeCard

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       <Navbar />
 
       <section className="main-section">
-        <div className="page-heading">
+        <div className="page-heading py-16">
           <h1>Track Your Applications & Resume Ratings</h1>
           <h2>Review Your submissions and check AI-powered feedback.</h2>
         </div>
